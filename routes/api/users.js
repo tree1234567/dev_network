2ec04e0d4df56bf6671afdded189baa73af5f2dd
Amador <aamador@stats.com>
2018-05-19 21:18:12 -0500

    Added passport Authorization

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys").secretOrKey;
+const passport = require("passport");
 // Load User model
 const User = require("../../models/User");
 
@@ -86,5 +87,17 @@ router.post("/login", (req, res) => {
     }
   });
 });
+
+//@route    Post api/users/current
+//@desc     Return current user
+//@access   Private
+
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json(req.user);
+  }
+);
 
 module.exports = router;
