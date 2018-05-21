3aaf4082df499c6eb00ceff6bbe816125b334ea2
Amador <aamador@stats.com>
2018-05-20 19:21:37 -0500

    Added post model

--- models/Post.js
+++ models/Post.js
@@ -4,6 +4,53 @@ const Schema = mongoose.Schema;
 const PostSchema = new Schema({
   user: {
     type: Schema.Types.ObjectId,
-    ref: "users"
+    ref: "users",
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String,
+    required: true
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        required: true
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      avatar: {
+        type: String,
+        required: true
+      },
+      date: {
+        type: Date.now,
+        required: true
+      }
+    }
+  ],
+  date: {
+    type: Date.now,
+    required: true
   }
 });
+
+module.exports = Post = mongoose.model("post", PostSchema);
