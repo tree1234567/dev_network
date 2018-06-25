0ab3dc0dff96fb5b841af4b63361948140a26d6d
Amador <aamador@stats.com>
2018-06-24 21:19:25 -0500

    Added components for the profile creation page

---
+++ client/src/components/common/InputGroup.js
@@ -0,0 +1,50 @@
+import React from "react";
+import classnames from "classnames";
+import PropTypes from "prop-types";
+
+const InputGroup = ({
+  name,
+  placeholder,
+  value,
+  error,
+  type,
+  icon,
+  onChange
+}) => {
+  return (
+    <div className="input-group mb-3">
+      <div className="input-group-prepend">
+        <span className="input-group-text">
+          <i className={icon} />
+        </span>
+      </div>
+      <textarea
+        name={name}
+        placeholder={placeholder}
+        className={classnames("form-control form-control-lg", {
+          "is-invalid": error
+        })}
+        onChange={onChange}
+        value={value}
+        disabled={disabled}
+      />
+      {error && <div className="invalid-feedback">{error}</div>}
+    </div>
+  );
+};
+
+InputGroup.propTypes = {
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+  error: PropTypes.string,
+  type: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired
+};
+
+InputGroup.defaultProps = {
+  type: "text"
+};
+
+export default InputGroup;
