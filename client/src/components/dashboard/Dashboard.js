d10d6b7e0d1c43132b585fe51f4798365d206caa
Amador <aamador@stats.com>
2018-06-24 20:35:41 -0500

    default dashboard message

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { getCurrentProfile } from "../../actions/profileActions";
 import Spinner from "../common/Spinner";
 
@@ -19,17 +20,26 @@ class Dashboard extends Component {
     if (profile === null || loading) {
       dashboardContent = <Spinner />;
     } else {
-      dashboardContent = <h1>HELLLOOOO</h1>;
+      if (Object.keys(profile).length > 0) {
+        dashboardContent = <h4>TODO: Display Profile</h4>;
+      } else {
+        dashboardContent = (
+          <div>
+            <p className="lead text-muted">Welcome, {user.name}</p>
+            <p>You have not yet setup a profile, please add some info</p>
+            <Link to="/create-profile" className="btn btn-lg btn-info">
+              Create Profile
+            </Link>
+          </div>
+        );
+      }
     }
     return (
       <div className="dashboard">
         <div classsName="container">
           <div className="row">
             <div className="col-md-12">
-              <div className="display-4">
-                <h1>Dashboard</h1>
-                {dashboardContent}
-              </div>
+              <div className="display-4">{dashboardContent}</div>
             </div>
           </div>
         </div>
